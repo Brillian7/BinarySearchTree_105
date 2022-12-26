c96f53249306bd185e8681f0763038b5b8b98955
Brilly <ASUS@LAPTOP-BEUE1DIV>
2022-12-26 17:43:32 +0700

    add public void inorder

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -72,6 +72,20 @@ namespace BinarySearchTree
                     currentnode = currentnode.lchild;
             }
         }
+        public void inorder(node ptr)
+        {
+            if(ROOT == null)
+            {
+                Console.WriteLine("tree is empty");
+                return ;
+            }
+            if(ptr != null)
+            {
+                inorder(ptr.lchild);
+                Console.WriteLine(ptr.info + "");
+                inorder(ptr.rchild);
+            }
+        }
     }
     class program
     {

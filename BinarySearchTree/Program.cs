73b58fe065b50d51c5492db1e5015b1ab0d1e671
Brilly <ASUS@LAPTOP-BEUE1DIV>
2022-12-26 17:58:42 +0700

    add case 1

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -131,7 +131,16 @@ namespace BinarySearchTree
                 Console.WriteLine("\nEnter your choice (1-5)");
                 char ch = Convert.ToChar(Console.ReadLine());
                 Console.WriteLine();
-
+                switch (ch)
+                {
+                    case '1':
+                        {
+                            Console.WriteLine("Enter a word: ");
+                            string word = Console.ReadLine();
+                            x.insert(word);
+                        }
+                        break;
+                }
 
             }
         }

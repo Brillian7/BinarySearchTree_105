322c1fbada606124c377a616a496e658d8e85a24
Brilly <ASUS@LAPTOP-BEUE1DIV>
2022-12-26 18:00:46 +0700

    ADD CASE 4

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -150,6 +150,11 @@ namespace BinarySearchTree
                             x.preorder(x.ROOT);
                         }
                         break;
+                    case '4':
+                        {
+                            x.postorder(x.ROOT);
+                        }
+                        break;
                 }
 
             }
